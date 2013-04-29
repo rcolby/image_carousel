b959bb65d1de301594740fd693a7392ba706feeb
Rob Colby <rcolby@me.com>
2013-04-28 17:06:59 -0700

    Got image swap working.

--- carousel.js
+++ carousel.js
@@ -1,3 +1,16 @@
-var carousel = document.getElementById = 'carousel';
-carousel.style.marginLeft = 0;
+var carousel = document.getElementById('carousel');
+carousel.style.marginLeft = '0';
 
+
+prevButton = document.getElementById('prev');
+nextButton = document.getElementById('next');
+
+
+
+nextButton.onclick = function() {
+  carousel.style.marginLeft = (parseInt(carousel.style.marginLeft, 10) - 525) + 'px';
+};
+
+prevButton.onclick = function() {
+  carousel.style.marginLeft = (parseInt(carousel.style.marginLeft, 10) + 525) + 'px';
+};
