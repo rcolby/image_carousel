ff523848e96b08b1a104b37ca16031ec4d72b6e6
Rob Colby <rcolby@me.com>
2013-04-28 17:52:25 -0700

    Image swap working for prev and next.

--- carousel.js
+++ carousel.js
@@ -1,4 +1,6 @@
 var carousel = document.getElementById('carousel');
+var leftPosition = 0;
+
 carousel.style.marginLeft = '0';
 
 
@@ -8,9 +10,21 @@ nextButton = document.getElementById('next');
 
 
 nextButton.onclick = function() {
-  carousel.style.marginLeft = (parseInt(carousel.style.marginLeft, 10) - 525) + 'px';
+  if(leftPosition > -1049) {
+    carousel.style.marginLeft = (parseInt(carousel.style.marginLeft, 10) - 525) + 'px';
+    leftPosition -= 525;
+  } else {
+    carousel.style.marginLeft = '0';
+    leftPosition = 0;
+  }
 };
 
 prevButton.onclick = function() {
-  carousel.style.marginLeft = (parseInt(carousel.style.marginLeft, 10) + 525) + 'px';
+  if(leftPosition === 0) {
+    carousel.style.marginLeft = '-1050px';
+    leftPosition = -1050;
+  } else {
+    carousel.style.marginLeft = (parseInt(carousel.style.marginLeft, 10) + 525) + 'px';
+    leftPosition += 525;
+  }
 };
